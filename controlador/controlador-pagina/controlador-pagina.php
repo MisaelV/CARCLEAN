7ec5155495ee8bce269c24643b4671d7acc2f7bc
MisaelV <victormisael.martinez.ventura@alumno.uttehuacan.edu.mx>
2018-05-23 01:40:23 -0500

    se creo controlador página

---
+++ controlador/controlador-pagina/controlador-pagina.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ * Description of controlador-pagina
+ *
+ * @author VICTOR
+ */
+class controlador-pagina {
+//put your code here
+}
