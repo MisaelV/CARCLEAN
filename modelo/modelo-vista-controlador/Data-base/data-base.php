bd288dd5f912df66cf48bd478dbe6bf9cc5cad06
Omar <Omar@Tormenta>
2018-05-24 20:17:18 -0500

    conecciones individuales-base de datos

---
+++ modelo/modelo-vista-controlador/Data-base/data-base.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ * Description of data-base
+ *
+ * @author Omar
+ */
+class data-base {
+        public static function conexion(){
+        $conexion=new mysqli("localhost", "root", "", "mvc");
+        $conexion->query("SET NAMES 'utf8'");
+        return $conexion;
+    }
+}
+?>
