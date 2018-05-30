b19ba7f9598fc6f638def44e5a3701cfe7b97817
John Wezka <John Wezka@DESKTOP-MRVLTP0>
2018-05-30 15:03:44 -0500

    Cambio de color e informacion de la plantilla de inicio.

--- vista/page/index.php
+++ vista/page/index.php
@@ -4,9 +4,9 @@ include_once './modelo/BD.php';
 include_once './modelo/modelo-pagina/modelo-pagina.php';
 include_once './controlador/controlador-pagina/controlador-pagina.php';
 /**
-  INICIALIZA CONTROLADOR
+INICIALIZA CONTROLADOR
  */
-$controlador = new controladorPagina();
+$controlador= new controladorPagina();
 ?>
 <html>
     <head>
@@ -16,18 +16,17 @@ $controlador = new controladorPagina();
         <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
         <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:400,300|Raleway:300,400,900,700italic,700,300,600">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">    
         <link rel="stylesheet" type="text/css" href="vista/page/css/jquery.bxslider.css">
         <link rel="stylesheet" type="text/css" href="vista/page/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="vista/page/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="vista/page/css/animate.css">
         <link rel="stylesheet" type="text/css" href="vista/page/css/style.css">
-
+       
     </head>
 
     <body>
-        <!--#025A9  Color de página-->
+   <!--#025A9  Color de página-->
         <div class="loader"></div>
         <div id="myDiv">
             <!--HEADER-->
@@ -49,9 +48,10 @@ $controlador = new controladorPagina();
                                 <!-- Barra de menu general, que enlaza a diferentes paginas del proyecto o externas.-->
                                 <div class="collapse navbar-collapse" id="myNavbar">
                                     <ul class="nav navbar-nav navbar-right">
-                                        <li class="active"><a href="#main-header">INICIO</a></li>
-                                        <li class=""><a href="#servicios">SERVICIOS</a></li>
-                                        <li class=""><a href="#beneficios">Beneficios</a></li>
+                                        <li class="active"><a href="#main-header">Home</a></li>
+                                        <li class=""><a href="vista/page/">About</a></li>
+                                        <li class=""><a href="#service">Services</a></li>
+                                        <li class=""><a href="#contact">Contact Us</a></li>
                                         <li class=""><a href="#contact">Sing in</a></li>
                                     </ul>
                                 </div>
@@ -60,14 +60,14 @@ $controlador = new controladorPagina();
                         </nav>
                     </header>
                     <!-- SLOGAN DE LA PAGINA "CARECLEAN"-->
-
+                    
                     <div class="wrapper">
                         <div class="container">
                             <div class="row">
                                 <div class="banner-info text-center wow fadeIn delay-05s">
                                     <h1 class="service-title pad-bt15">CARE<span class="logo-dec">CLEAN</span></h1>    
                                     <h2 >Con careclean siempre tendras tu auto limpio, desde la comodidad de tu hogar u oficina</h2>
-
+                                    
                                     <!--SLAIDER DE IMAGENES QUE REPRESENTEN EL TRABAJO O SERVICIOS OFRECIDOS POR LA EMPRESA RECIENTEMENTE-->
                                     <section id="portfolio" class="section-padding wow fadeInUp delay-05s">
                                         <div class="container">
@@ -131,7 +131,7 @@ $controlador = new controladorPagina();
                                         </div>
                                         <div class="brn-btn">
                                             <a  href="#" class=" btn btn-more"> Registrarme</a>
-
+                                            
                                         </div>
                                     </section>
                                 </div>
@@ -145,110 +145,159 @@ $controlador = new controladorPagina();
             </div>
             <!---->
             <!-- APARTADO DE SERVICIOS  DE LA PAGINA -->
-            <section id="servicios" class="section-padding wow fadeIn delay-05s">
+            <section id="feature" class="section-padding wow fadeIn delay-05s">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12 text-center">
-                            <h2 class="service-title pad-bt15">Nuestros servicios</h2>
+                            <h2 class="service-title pad-bt15">What We Do?</h2>
+                            <p class="sub-title pad-bt15">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br>tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <hr class="bottom-line">
                         </div>
                         <div class="col-md-4 col-sm-6 col-xs-12">
                             <div class="wrap-item text-center">
                                 <div class="item-img">
                                     <!--ICONO REPRESENTATIVO DE EL SERVICIO OFRECIDO-->
-                                    <img src="vista/page/ImagenesCW/Exterior.jpg">
+                                    <img src="vista/page/ImagenesCW/003-lavado-de-coches_1.png">
                                 </div>
-                                <h3 class="pad-bt15">Lavado completo del exterior</h3>
-                                <p>Cuando conduce su automóvil sin una limpieza por mucho tiempo, no pasará mucho tiempo hasta que alguien escriba "Láveme" en él. Para evitar eso y hacer un mantenimiento adecuado para el aspecto de su auto, ¡asegúrese de lavar regularmente el exterior!.</p>
+                                <h3 class="pad-bt15">Lavado</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             </div>
                         </div>
                         <div class="col-md-4 col-sm-6 col-xs-12">
                             <div class="wrap-item text-center">
                                 <div class="item-img">
                                     <!--ICONO REPRESENTATIVO DE EL SERVICIO OFRECIDO-->
-                                    <img src="vista/page/ImagenesCW/interiror.jpg">
-                                </div>
-                                <h3 class="pad-bt15">Lavado completo de interiores</h3>
-                                <p>Limpiar el interior de su vehículo es tan importante como hacer el mantenimiento exterior, si no más. Esto significa que cuando ha pasado demasiado tiempo desde la última vez que ordenó realizar una limpieza, ¡es hora de hacerlo ahora mismo!</p>
-                            </div>
-                        </div>
-                        <div class="col-md-4 col-sm-6 col-xs-12">
-                            <div class="wrap-item text-center">
-                                <div class="item-img">
-                                    <!--ICONO REPRESENTATIVO DE EL SERVICIO OFRECIDO-->
-                                    <img src="vista/page/ImagenesCW/encerado.jpg">
+                                    <img src="vista/page/ImagenesCW/004-barita-magica_1.png">
                                 </div>
                                 <h3 class="pad-bt15">Encerado</h3>
-                                <p>Cuando hablamos de cuidar adecuadamente su vehículo, los detalles automáticos vienen a la mente de inmediato. Es una de las partes más importantes de este servicio de cuidado del automóvil y está diseñada para hacer que la superficie de su coche sea nítida y bien pulida, ¡tan brillante como brillante!</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                             </div>
                         </div>
-
-
+                        <div class="col-md-4 col-sm-6 col-xs-12">
+                            <div class="wrap-item text-center">
+                                <div class="item-img">
+                                    <!--ICONO REPRESENTATIVO DE EL SERVICIO OFRECIDO-->
+                                    <img src="vista/page/ImagenesCW/001-soltar_1.png">
+                                </div>
+                                <h3 class="pad-bt15">Limpieza</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </section>
             <!---->
-
-            <!--APARTADO DE BENEFICIOS-->
-            <section id="beneficios" class="section-padding wow fadeInUp delay-05s">
+            
+             <!--APARTADO DE BENEFICIOS-->
+            <section id="service" class="section-padding wow fadeInUp delay-05s">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12 text-center">
                             <h2 class="service-title pad-bt15">Nuestros Beneficios</h2>
+                            
+                            <p class="sub-title pad-bt15">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br>tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <hr class="bottom-line">
                         </div>
                         <div class="col-md-3 col-sm-6 col-xs-12">
                             <div class="service-item">
-                                <img src="vista/page/ImagenesCW/006-servicio-al-cliente.png">
-                                <h3>Recerva facil.</h3>
-                                <p>Lo primero es que tendrá que reservar una cita con nosotros o simplemente visitarnos para ver si tenemos un espacio vacante.</p>
-
+                                <h3><span>A</span>horro de Agua</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <a href="">learn more...</a>
                             </div>
                         </div>
                         <div class="col-md-3 col-sm-6 col-xs-12">
                             <div class="service-item">
-                                <img src="vista/page/ImagenesCW/007-mantenimiento.png">
-                                <h3>Perfecto Servicio</h3>
-                                <p>Cuando su auto ya esté en nuestro lugar, todo lo que tendrá que hacer es esperar un poco hasta que salga todo brillante y limpio.</p>
-
+                                <h3><span>A</span>horro de Combustible</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <a href="">learn more...</a>
                             </div>
                         </div>
                         <div class="col-md-3 col-sm-6 col-xs-12">
                             <div class="service-item">
-                                <img src="vista/page/ImagenesCW/005-servicio.png">
-                                <h3>Servicios adicionales</h3>
-                                <p>Además de proporcionar los mejores servicios de lavado exterior e interior. ¡también ofrecemos encerado, detallado y aspirado!</p>
-
+                                <h3><span>A</span>horro de Tiempo</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <a href="">learn more...</a>
                             </div>
                         </div>
                         <div class="col-md-3 col-sm-6 col-xs-12">
                             <div class="service-item">
-                                <img src="vista/page/ImagenesCW/004-etiqueta-del-precio.png">
-                                <h3>Precios increibles</h3>
-                                <p>
-                                    Con todos nuestros servicios siendo realmente de primera categoría, aquí también está la última alegría: ¡el mejor precio!.</p>                                
+                                <h3><span>D</span>igital Marketing</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <a href="">Learn more...</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
             <!---->
-
-            <!--APARTADO DE CONTACTO-->
+            
+            <!--APARTADO DE CONTACTO "FOOTER"-->
             <section id="contact" class="section-padding wow fadeInUp delay-05s">
                 <div class="container">
                     <div class="row">
                         <!--APARTADO PARA MANTENERCE EN CONTACTO -->
-                        <div class="col-md-12 text-center white">                            
-                               
-                        </div>                                                
+                        <div class="col-md-12 text-center white">
+                            <h2 class="service-title pad-bt15">Keep in touch with us</h2>
+                            <p class="sub-title pad-bt15">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br>tempor incididunt ut labore et dolore magna aliqua.</p>
+                            <hr class="bottom-line white-bg">
+                        </div>
+                        <!--INFORMACION DE CONTACTO -->
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="loction-info white">
+                                <p><i class="fa fa-map-marker fa-fw pull-left fa-2x"></i>A99 Adam Street<br>Texas, TX 555072</p>
+                                <p><i class="fa fa-envelope-o fa-fw pull-left fa-2x"></i>info@baker.com</p>
+                                <p><i class="fa fa-phone fa-fw pull-left fa-2x"></i>+41 5787 2323</p>
+                            </div>
+                        </div>
+                        <!--APARTADO PARA EL FORMULARIO -->
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="contact-form">
+                                <div id="sendmessage">Your message has been sent. Thank you!</div>
+                                <div id="errormessage"></div>
+                                <form action="" method="post" role="form" class="contactForm">
+                                    <div class="col-md-6 padding-right-zero">
+                                        <div class="form-group">
+                                            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                            <div class="validation"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                            <div class="validation"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                                            <div class="validation"></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                                            <div class="validation"></div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-submit">SEND NOW</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
             <!---->
+            
             <!--APARTADO DE DERECHOS RESERVADOS-->
             <footer id="footer">
                 <div class="container">
                     <div class="row text-center">
-
+                        <p>&copy; CARECLEAN Theme. All Rights Reserved.</p>
+                        <div class="credits">                            
+                            Designed by <a href="#">Innovative Application</a>
+                        </div>
                     </div>
                 </div>
             </footer>
