29794d617920a1d77c24353c14632792321916b7
BalmesYess <victormisael.martinez.ventura@alumno.uttehuacan.edu.mx>
2018-07-26 22:44:48 -0500

    26/07/2018 proyecto reestructurado

--- customer/index.php
+++ customer/index.php
@@ -4,11 +4,12 @@
       <title>Inicio de Sesión</title>
         <meta charset="UTF-8">
         <link rel="icon" type="image/png" href="images/logo.png">
-         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
         <meta name="viewport" content="width=device-width, initial-scale=1">	
         <link rel="stylesheet" type="text/css" href="../carclean/management/assets/css/util.css">
         <link rel="stylesheet" type="text/css" href="../carclean/management/assets/css/main.css">
         <link rel="stylesheet" type="text/css" href="../settings/style-web/css.css">
+                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
     </head>
     <body>
         <!--Inicia el cuerpo del contenido-->
@@ -24,34 +25,32 @@
                         </center>
                     </div>
                     <div class="form-group col-md-12">
-                        <label class="col-form-label">Nombre de usuario: </label>
-                        <input type="email" name="usuario" class="form-control" id="user" aria-describedby="emailHelp" placeholder="Enter email" required="">
+                        <label class="col-form-label">Nombre de usuario o email: </label>
+                        <input type="text" name="usuario" class="form-control" id="user" aria-describedby="emailHelp" placeholder="User or email" required="">
                     </div>
                     <div class="form-group col-md-12">
-                        <label class="col-form-label">Password:</label>
+                        <label class="col-form-label">Contraseña:</label>
                         <input type="password" name="contraseña" class="form-control" id="password" placeholder="Password" required="">
                     </div>
 
                     <div class="form-group col-md-12 ">
                         <center>
-                    <button type="submit" class="btn btn-primary">Entrar</button>
+                    <button  type="button" class="btn btn-primary" onclick="location.href = 'account/profile.html'">Entrar</button>
                         </center>
-                    </div>
-                    <div class="form-group col-md-12 ">
                         <center>
-                            <label class="col-form-label">¿No tienes una cuenta? Registrate <a href="register.php" style="color: darkblue"><h5>aquí</h5></a></label>
+                            <label class="col-form-label">¿No tienes una cuenta? <a href="register.php" style="color: #0370CB;font-family: sans-serif;"><h5>Crear una</h5></a></label>
                         </center>
-                    </div>
-                    
-                    <center>
+                         <center>
                         <p style="text-">© 2018 <a href="#" >Carclean.</a></p>
                     </center>
+                    </div>
+                   
                 </form>
             </div>
                  <!--Termina el recuadro de login-->
             </div>
         <!--Termina el cuerpo del contenido-->
              <div class="limpiar"></div>
-   
+  
     </body>
 </html>
